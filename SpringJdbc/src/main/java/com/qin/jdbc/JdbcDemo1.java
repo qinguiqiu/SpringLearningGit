871a77eced185f0610f7caa12ce26a1d3b108aa3
g2 <113022315@qq.com>
2020-10-16 16:49:18 +0800

    first use spring

---
+++ SpringJdbc/src/main/java/com/qin/jdbc/JdbcDemo1.java
@@ -0,0 +1,24 @@
+package com.qin.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class JdbcDemo1 {
+    public static void main(String[] args) throws Exception{
+        //注册链接
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        //获取连接
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin", "root", "qgq950929");
+        //获取操作数据库的预处理对象
+        PreparedStatement pstm = conn.prepareStatement("select * from account");
+        ResultSet rs = pstm.executeQuery();
+        while(rs.next()){
+            System.out.println(rs.getString("UID"));
+        }
+        rs.close();
+        pstm.close();
+        conn.close();
+    }
+}
